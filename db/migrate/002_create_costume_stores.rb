02518a39317d25f93801e4fce89053a94542a5f9
norman lewis <takialog@gmail.com>
2018-03-07 05:51:08 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,9 @@ class CreateCostumeStores << ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :employess
-      t.boolean :business
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
     end
