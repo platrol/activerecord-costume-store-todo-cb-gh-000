3369b62fcfbde01dd1be06a55e4fec2d6c057951
norman lewis <takialog@gmail.com>
2018-03-07 06:19:34 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.float :price
-      t.boolean :famliy_friendly
+      t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :description
